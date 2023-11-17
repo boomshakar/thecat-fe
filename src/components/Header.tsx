bb46e874f444903e59e44aec11fd6df89ba9e6e5
Oluwafemi Ṣosanya <Hefehmzy@gmail.com>
2023-11-17 15:56:34 +0000

    update: index page header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,15 @@
+import leadBannerImg from "../assets/image/lead_home_banner.webp";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="head_banner">
+      <div className="head_banner_img">
+        <img src={leadBannerImg} alt="lead-img" />
+      </div>
+      <div className="head_banner_content text-white text-center">
+        <h1 className="font-lato font-700">TOYS FOR YOUR PET</h1>
+        <p className="font-300">Style in handcrafts, made by professionals for you</p>
+      </div>
+    </header>
+  );
 };
